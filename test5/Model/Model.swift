f4b72ce3d47aaccb78909a3ef815c7693a75f22a
OldBegin <ygyou@me.com>
2020-02-01 20:58:02 +0800

    Hello World!!!!!!!!

---
+++ test5/Model/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  test5
+//
+//  Created by Youngun on 2020/02/01.
+//  Copyright © 2020 Youngun. All rights reserved.
+//
+
+import Foundation
+
+class Memo {
+    var content : String
+    var insertDate : Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Momo1"),
+        Memo(content: "Momo2")
+    ]
+}
